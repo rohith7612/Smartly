2e2fcc4956e284e01058b7835c8af762041803c6
rohith7612 <prudhvirohith45@gmail.com>
2025-10-24 23:11:30 +0530

    Allow all hosts.

--- smartly/settings.py
+++ smartly/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9mb^wy$r4zyiqe$9@+#3ec7tn^+7t*68_f$1$!lk#pk5)mug)j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['smartly-nlq7.onrender.com', 'localhost', '127.0.0.1', 'smartly.ai']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1', 'smartly.ai']
 
 
 
